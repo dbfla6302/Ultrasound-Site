9cab34b16ecf5bd8224c0136c0aacb11e8dec1c5
MAENG YU RIM <87506702+dbfla6302@users.noreply.github.com>
2023-07-23 23:52:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ $(function(){
       if (window.innerWidth > 960) {  // 다바이스 크기가 1024이상일때
         $("header .gnb").show();
         $("#main-gnb li").off();
-        $("#main-gnb li").hover(function(){
-          $(".gnb-detail").show();
+
+        // skip-navigation
+        $("#skip-navigation p").bind("focusin", function() {
+          $(this).addClass('on').siblings('p').removeClass('on');
+        });
+        $("#skip-navigation p").bind("focusout", function() {
+          $("#skip-navigation p").removeClass('on');
+        });
+
+        // main-gnb
+        $("#main-gnb li").bind("mouseenter focusin", function() {
+          $(".gnb-detail").css({"opacity":1, "z-index":10});
           $(this).addClass("on").children(".gnb-detail").addClass("on");
-        }, function(){
-          $(".gnb-detail").hide();
+        });
+        $("#main-gnb li").bind("mouseleave focusout", function() {
+          $(".gnb-detail").css({"opacity":0, "z-index":-1});
           $(this).removeClass("on").children(".gnb-detail").removeClass("on");
         });
 
@@ -30,7 +41,8 @@ $(function(){
 });
 
 // slide
-new Swiper('.notice-sldie .swiper-container', {
+var swiper_1, swiper_2, swiper_3;
+swiper_1 = new Swiper('.notice-sldie .swiper', {
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
@@ -39,13 +51,18 @@ new Swiper('.notice-sldie .swiper-container', {
     keyboard: true, 
     slidesPerView: 1,
     centeredSlides: true, 
+    observer: true,
+    observeParents: true,
     pagination: {
       el: '.notice-sldie .swiper-pagination',
       clickable: true,
+      renderBullet : function (index, className) {  // className이 기본값이 들어가게 필수 설정
+        return '<button class="' + className + '">' + (index + 1) + '</button>'
+      },
     },
 });
 
-new Swiper('.guide-sldie .swiper-container', {
+swiper_2 = new Swiper('.guide-sldie .swiper', {
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
@@ -60,7 +77,7 @@ new Swiper('.guide-sldie .swiper-container', {
     }, 
 });
 
-new Swiper('.bottome-sldie .swiper-container', {
+swiper_3 = new Swiper('.bottome-sldie .swiper', {
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
